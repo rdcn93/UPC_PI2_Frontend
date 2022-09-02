9b4ee5674ff07fe136380865a3d597f36be9b2ec
Raul Castañeda Najar <digiespiri93@hotmail.com>
2022-09-02 10:36:14 -0500

    Creación de componentes Cliente, Almacen, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,17 @@ import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { AlmacenComponent } from './components/almacen/almacen.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { ProveedorComponent } from './components/proveedor/proveedor.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'almacen', component: AlmacenComponent, canActivate: [AuthGuard] },
+  { path: 'cliente', component: ClienteComponent, canActivate: [AuthGuard] },
+  { path: 'producto', component: ProductoComponent, canActivate: [AuthGuard] },
+  { path: 'proveedor', component: ProveedorComponent, canActivate: [AuthGuard] },
   { path: 'usuario', component: UsuarioComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
 ];
@@ -29,7 +37,11 @@ export function tokenGetter() {
     AppComponent,
     HomepageComponent,
     LoginComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    ProductoComponent,
+    AlmacenComponent,
+    ClienteComponent,
+    ProveedorComponent
   ],
   imports: [
     BrowserModule,
